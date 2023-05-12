94a27c275ef17d56e81112d5cbb2e48968140508
LI Simon <SL236797@intra.cea.fr>
2023-05-12 14:09:47 +0200

    dialog manager

---
+++ script/DialogManager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var file:Resource
+@onready var dialog = load(file.resource_path)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _unhandled_input(event) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		
