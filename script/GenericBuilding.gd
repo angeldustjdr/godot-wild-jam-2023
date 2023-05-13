8194f15579eb5e694bd0f13c22f70c18b6e5f0e2
LI Simon <SL236797@intra.cea.fr>
2023-05-13 21:37:05 +0200

    redimension

--- script/GenericBuilding.gd
+++ script/GenericBuilding.gd
@@ -2,11 +2,10 @@ extends Area2D
 class_name GenericBuilding
 
 @export var description:String = "A generic building."
-@onready var tooltip = $Tooltip
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tooltip.tooltip_text = description
+	$Tooltip.tooltip_text = description
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
