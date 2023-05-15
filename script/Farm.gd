719de693f7f3a1bf73d7a784fb94f151ac28ffe1
MetalGanon <georis.billo@gmail.com>
2023-05-15 22:33:03 +0200

    first try patterns

--- script/Farm.gd
+++ script/Farm.gd
@@ -1,4 +1,5 @@
 extends GenericBuilding
+class_name Farm
 
 func _on_input_event(_viewport, _event, _shape_idx):
 	pass
--- script/Main.gd
+++ script/Main.gd
@@ -5,6 +5,7 @@ var selected # hold the cell to build onto
 func _ready(): # signal connexion
 	RadioDiffusion.connect("cleanSelectionNeeded",cleanSelected)
 	RadioDiffusion.connect("createBuildMenuNeeded",createBuildMenu)
+	$Grid.connect("gridUpdated",checkPatterns)
 	
 	GameState.ressourceInit(1000,0,0,0)
 
@@ -13,6 +14,11 @@ func createBuildMenu(obj):
 	var b = load("res://scene/BuildMenu.tscn").instantiate()
 	b.position = get_local_mouse_position()
 	add_child(b)
+
+func checkPatterns(i,j):
+	for pattern in $Patterns.get_children():
+		pattern.check(i,j)
+	pass
 
 func cleanSelected():
 	selected = null
