0ffbdc3a7e44fa1b696a97352308efffecfe1bb7
LI Simon <SL236797@intra.cea.fr>
2023-05-20 20:30:44 +0200

    menu

--- script/BuildMenu.gd
+++ script/BuildMenu.gd
@@ -9,6 +9,9 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index==MOUSE_BUTTON_RIGHT and event.pressed:
 			freeMenu()
+		if event.button_index==MOUSE_BUTTON_LEFT and event.pressed:
+			if get_global_mouse_position().x < global_position.x or get_global_mouse_position().x > global_position.x+size.x or get_global_mouse_position().y < global_position.y or get_global_mouse_position().y > global_position.y+size.y:
+				freeMenu()
 
 func _on_farm_button_pressed():
 	if main.selected != null:
@@ -29,6 +32,7 @@ func _on_well_button_pressed():
 
 
 func _on_cancel_pressed():
+	GameState.menuOpened = false
 	freeMenu()
 
 func freeMenu():
--- script/ConfirmMenu.gd
+++ script/ConfirmMenu.gd
@@ -9,6 +9,9 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index==MOUSE_BUTTON_RIGHT and event.pressed:
 			freeMenu()
+		if event.button_index==MOUSE_BUTTON_LEFT and event.pressed:
+			if get_global_mouse_position().x < global_position.x or get_global_mouse_position().x > global_position.x+size.x or get_global_mouse_position().y < global_position.y or get_global_mouse_position().y > global_position.y+size.y:
+				freeMenu()
 
 func _on_destroy_pressed():
 	if main.selected != null:
@@ -16,11 +19,11 @@ func _on_destroy_pressed():
 	freeMenu()
 
 func _on_cancel_pressed():
+	GameState.menuOpened = false
 	freeMenu()
 
 func freeMenu():
 	GameState.menuOpened = false
 	queue_free()
-
 
 
