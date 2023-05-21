f8d440a9d883ae40b31e8ae38bb4e78214b4e190
LI Simon <SL236797@intra.cea.fr>
2023-05-21 13:17:42 +0200

    casserolade

--- script/Logo.gd
+++ script/Logo.gd
@@ -4,6 +4,6 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SoundManager.playSoundNamed("casserole")
-	await get_tree().create_timer(4.0).timeout
+	await get_tree().create_timer(1.5).timeout
 	$Rideau.actionRideau("out")
 	get_tree().change_scene_to_file("res://TitleScene.tscn")
