64be25821bfea2b82a23e65f4336556603df4a2d
LI Simon <SL236797@intra.cea.fr>
2023-05-21 19:30:23 +0200

    poooooouuuuule

--- script/CreditAnimation.gd
+++ script/CreditAnimation.gd
@@ -4,7 +4,7 @@ extends Control
 
 func _ready():
 	var myTween = create_tween()
-	myTween.tween_property(self, "position", Vector2(0,-self.size.x), 15.0)
+	myTween.tween_property(self, "position", Vector2(0,-2*self.size.x), 20.0)
 	
 func _input(event):
 	if event is InputEventMouseButton:
