9816a10daa0b195cfc709cad8af9bac843498e02
MetalGanon <georis.billo@gmail.com>
2023-05-21 19:17:10 +0200

    MOTHERFUCKING FRENCH PAN STUDIO

--- script/Logo.gd
+++ script/Logo.gd
@@ -1,10 +1,15 @@
 extends Control
 
+var nombre_de_coup_de_casserole = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SoundManager.playSoundNamed("casserole")
-	await get_tree().create_timer(2.0).timeout
+	for i in range(0,nombre_de_coup_de_casserole):
+		$Sprite2D/AnimationPlayer.play("ping")
+		await get_tree().create_timer(0.1).timeout 
+		SoundManager.playSoundNamed("casserole_one_hit")
+		await $Sprite2D/AnimationPlayer.animation_finished
+	await get_tree().create_timer(0.2).timeout 
 	$Rideau.actionRideau("out")
 	#SoundManager.playSoundNamed("transition")
 	get_tree().change_scene_to_file("res://TitleScene.tscn")
